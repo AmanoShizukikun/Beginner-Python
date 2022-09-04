e1436eb54b18ce3e6d5410620f0083283da3d377
AmanoShizukikun <hackamsk@gmail.com>
2022-09-05 03:01:13 +0800

    Beginner Python 2022/9/5

---
+++ guessing_game.py
@@ -0,0 +1,30 @@
+#猜數字遊戲
+#1.數字範圍0~99
+#2.每一次都會給大一點或小一點的提示
+#3.猜錯三次就輸了
+#4.要用到while迴圈
+
+#我的解法
+import random
+
+secret_num = (random.randint(0,99))
+guess = None
+guess_count = 0
+guess_limit = 3
+out_of_limit = False
+
+while secret_num != guess and not(out_of_limit):
+    guess_count += 1
+    if guess_count <= guess_limit:
+        guess = int(input("請輸入數字: "))
+        if guess > secret_num:
+            print("小一點")
+        elif guess < secret_num:
+            print("大一點")
+    else:
+        out_of_limit = True
+
+if out_of_limit:
+    print("你輸了")
+else:
+    print("你贏了")
---
+++ while.py
@@ -0,0 +1,8 @@
+#while迴圈
+#當判斷為False之前會不停地重複
+i = 1
+while i <=5:
+    print(i)
+    i = i+1
+
+print("迴圈結束")
